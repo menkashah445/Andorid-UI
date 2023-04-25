3063fecb7fdf3a9fb72c77fd9282472144fbdf49
Menka shah <105760147+shahmenka@users.noreply.github.com>
2023-04-25 01:50:03 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.taskui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.taskui.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
