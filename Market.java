77fd94515300315a122e1cd0e091819ae8fa3231
Menka shah <105760147+shahmenka@users.noreply.github.com>
2023-04-25 01:50:23 -0400

    Add files via upload

---
+++ Market.java
@@ -0,0 +1,16 @@
+package com.example.taskui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taskui.R;
+
+public class Market extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market);
+    }
+}
